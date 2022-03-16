922156f868a1322d27ab3e4d0a5f4b1071011d6e
unknown <gillellajagadeesh28971@gmail.com>
2022-03-16 18:11:20 +0530

    added changes

---
+++ src/arthameticcaluclator/caluclator.java
@@ -0,0 +1,24 @@
+package arthameticcaluclator;
+
+public class caluclator {
+
+	public static void main(String[] args) {
+		
+		int number1=15,number2=3;
+		int sum = number1+number2;
+		System.out.println("Sum is:"+sum);
+		int dif = number1-number2;
+		System.out.println("Difference is:"+dif);
+		int mul = number1*number2;
+		System.out.println("Multiplied value is:"+mul);
+		int div = number1/number2;
+		System.out.println("Quotient is :"+div);
+		int rem = number1%number2;
+		System.out.println("Remainder is:"+rem);		
+		
+		
+
+
+	}
+
+}
